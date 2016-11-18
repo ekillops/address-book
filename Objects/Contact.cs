645c7fc7b79b395365c156935821a0211cbbbaf2
Erik Killops <erik.killops@gmail.com>
2016-11-18 08:19:25 -0800

    create Contact class object page

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -2,5 +2,62 @@ using System.Collections.Generic;
 
 namespace AddressBook.Objects
 {
-  
+  public class Contact
+  {
+    private string _firstName;
+    private string _lastName;
+    private string _phoneNumber;
+    private Address _address;
+
+    private int _id;
+
+    private static List<Contact> _instances = new List<Contact> {};
+
+    public Contact(string newFirstName, string newLastName, string newPhoneNumber, Address newAddress = new Address("street address","city","state","zip code"))
+    {
+      _firstName = newFirstName;
+      _lastName = newLastName;
+      _phoneNumber = newPhoneNumber;
+      _address = newAddress;
+
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
+
+    public void SetFirstName(string newFirstName)
+    {
+      _firstName = newFirstName;
+    }
+    public string GetFirstName()
+    {
+      return _firstName;
+    }
+    public void SetLastName(string newLastName)
+    {
+      _lastName = newLastName;
+    }
+    public string GetLastName()
+    {
+      return _lastName;
+    }
+
+    public void SetPhoneNumber(string newPhoneNumber)
+    {
+      _phoneNumber = newPhoneNumber;
+    }
+    public string GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+  }
 }
